5dbf6287bbf9e1373d209e95fff4f9351236301e
Rpa1Labs <romain.pajon@etu.univ-orleans.fr>
2022-11-25 19:00:28 +0100

    Fix String comparison issues

--- app/src/main/java/com/example/polyapp/edt/Specialities.java
+++ app/src/main/java/com/example/polyapp/edt/Specialities.java
@@ -79,7 +79,7 @@ public class Specialities {
     public int getIDBySpeciality(String speciality){
         int index = -1;
         for(int i=0; i<keys.length;i++){
-            if(speciality == keys[i]){
+            if(speciality.equals(keys[i])){
                 index = i;
                 break;
             }
